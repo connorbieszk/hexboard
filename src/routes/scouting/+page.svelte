12befb8af69970ab2e1f0319d96f132e1012822a
Connor PB B. <98125183+connorbieszk@users.noreply.github.com>
2026-05-06 15:44:42 +0000

    Add basic navigation

---
+++ src/routes/scouting/+page.svelte
@@ -0,0 +1,35 @@
+<div class="content">
+    <h1 style = "
+    background-color: var(--bg-color-3);
+    padding: 2vh;
+    margin: 2vh;
+    ">
+        Welcome Scouter!
+    </h1>
+
+    <p>
+        Please select a type of scouting below.
+    </p>
+    <div style =
+     "background-color: transparent;
+      display: flex;
+      margin: 2vh;
+      justify-items: center;
+      flex-direction: row;
+    ">
+     <a href="/scouting/match" class="button">
+            Match Scout
+        </a>
+        <a href="/scouting/practice" class="button" style = "
+        margin-left: auto;
+        ">
+            Practice Scout
+        </a>
+        <a href="/scouting/pit" class="button-right">
+            Pit Scout
+        </a>
+        <a href="/scouting/prematch" class="button-right">
+            Pre Scout
+        </a>
+    </div>
+</div>
